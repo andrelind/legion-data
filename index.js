4e616c1947ae48e100c324b49a9716e81ccbb90b
André Lind <andre.lind@smartrefill.se>
2018-06-07 14:00:49 +0200

    Add types to export

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const units = require("./units");
-const upgrades = require("./upgrades");
+const units = require('./units');
+const upgrades = require('./upgrades');
+export const types = require('./types');
 
 module.exports = {
   units,
-  upgrades
+  upgrades,
 };
