c1cce94521cc8107ecf75c4fe68eb5eab2f3d8a3
Steve Good <sgood@lanctr.com>
2019-10-22 21:23:31 -0700

    #27 - Added BARC RPS-6 Gunnerand BARC Ion Gunner

--- upgrades/gunner.js
+++ upgrades/gunner.js
@@ -47,7 +47,7 @@ const upgrades: Upgrade[] = [
     slot: 'Gunner',
     description: 'Reduce your maximum speed by 1.',
     restrictions: {
-      name: 'BARC Speeder',
+      name: 'BARC Speeder only',
       ldf: 'barcspeeder',
     },
     keywords: [
@@ -59,6 +59,59 @@ const upgrades: Upgrade[] = [
     weapon: {
       range: { from: 1, to: 3 },
       dice: { black: 2, white: 2 },
+    },
+  },
+  {
+    ldf: 'barcrps6gunner',
+    name: 'BARC RPS-6 Gunner',
+    points: 24,
+    slot: 'Gunner',
+    description: 'Reduce your maximum speed by 1.',
+    restrictions: {
+      name: 'BARC Speeder only',
+      ldf: 'barcspeeder',
+    },
+    keywords: [
+      {
+        name: 'Impact 2',
+        description:
+          'While attacking a unit that has Armor, change up to 2 [Hit] results to [Crit] results',
+      },
+    ],
+    weapon: {
+      range: { from: 2, to: 4 },
+      dice: { red: 1, black: 1, white: 1 },
+    },
+  },
+  {
+    ldf: 'barciongunner',
+    name: 'BARC Ion Gunner',
+    points: 28,
+    slot: 'Gunner',
+    description: 'Reduce your maximum speed by 1.',
+    restrictions: {
+      name: 'BARC Speeder only',
+      ldf: 'barcspeeder',
+    },
+    keywords: [
+      {
+        name: 'Fixed: Front, Rear',
+        description: 'The defender must be inside your front or rear arc.',
+      },
+      {
+        name: 'Impact 1',
+        description:
+          'While attacking a unit that has Armor, change up to 1 [Hit] results to [Crit] results',
+      },
+      {
+        name: 'Ion',
+        description:
+          'A vehicle or droid wounded by this attack gains 1 ion token.',
+      },
+    ],
+    weapon: {
+      range: { from: 1, to: 3 },
+      dice: { black: 3, white: 1 },
     },
   },
 ];
