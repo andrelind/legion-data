0815de91074fa1c4d78cc2bb01f0adb5d79ad930
Steve Good <stephen.good@umusic.com>
2019-11-13 13:00:22 -0800

    Fixed duplicate variable name

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import upgradesImport from './upgrades';
 import commandCardsImport from './command-cards';
 import typesImport from './types';
 import ldfImport from './types/ldf';
-import sources from './sources';
+import sourcesImport from './sources';
 
 export const units = unitsImport;
 export const upgrades = upgradesImport;
 export const commandCards = commandCardsImport;
 export const types = typesImport;
 export const ldf = ldfImport;
-export const sources = sources;
+export const sources = sourcesImport;
 
 export default {
   units,
